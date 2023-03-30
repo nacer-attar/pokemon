d7bb9d82f708a542a55ea1e66c57ed0af8496393
Nacer ATTAR <nacer.attar@laplateforme.io>
2023-03-30 15:56:55 +0200

    pokemon init

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,22 @@
 class Pokémon:
-    def __init__ (self, name, lvl, atkpts):
+    def __init__ (self, name, lvl, atkpts, type):
         self._name = name
         self._hp = 100
         self.lvl = lvl
         self.atkpts = atkpts
         self.defpts = 0
+        self.type = type
 
-        
+    #set n get hp
+    def sethp (self, hp):
+        self._hp = hp
+
+    def gethp (self):
+        return self._hp
+    #set n get defpts
+    def setdefpts (self,defpts):
+        self.defpts = defpts
+
+    def getdefpts (self):
+        return self.defpts
+    
--- typefeu.py
+++ typefeu.py
@@ -1,14 +1,20 @@
 from pokemon import Pokemon
-from typeeau import eau
-from typeterre import terre
-from typenormal import normal
 
-class feu (Pokemon):
-    def __init__ (self, name, lvl, atkpts):
-        super().__init__(self, name, lvl, atkpts)
+class fire (Pokemon) :
+    def __init__(self, name, lvl, atkpts, type) -> None:
+        super().__init__(self, name, lvl, atkpts, type)
+        
+    def checktypefire(self) :  #vérifie si le mdp existe dans le json en comparant le hash du mdp du user avec les hashes enregistrés.
+        with open("typefeu.json", "r") as file:
+            if self._name + "\n" in file:
+                self._hp = 69
+                self.atkpts = 83
+                self.defpts = 69
+                self.type = 'fire'
 
-    def afficherstats (self):
-        print (f"Nom = {self._name}\n")
+    def weakness (self):
 
-f=feu()
-print (f.afficherstats())
+
+
+        
+
